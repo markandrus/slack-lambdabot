c09ebcda39ce0cf145a8b40fda3ecb55f126deb0
Mark Andrus Roberts <markandrusroberts@gmail.com>
2015-09-15 17:01:01 -0700

    Cleanup

--- src/Main.hs
+++ src/Main.hs
@@ -6,24 +6,22 @@ module Main (main) where
 import Codec.Binary.UTF8.String (decodeString)
 import Control.Lens (use)
 import Control.Monad.IO.Class (liftIO)
--- import Data.Char (readLitChar)
 import Data.Functor (void)
 import Data.Maybe (fromMaybe)
 import Data.Monoid ((<>))
-import Data.Text (Text, dropWhile, pack, unpack, stripPrefix)
+import Data.Text (Text, dropWhile, filter, pack, unpack, stripPrefix)
 import Data.Text.Lazy (toStrict)
 import Data.Text.Lazy.Builder (toLazyText)
-import Debug.Trace (trace)
 import HTMLEntities.Decoder (htmlEncodedText)
-import qualified HTMLEntities.Builder as HTMLEncoded
+-- import qualified HTMLEntities.Builder as HTMLEncoded
 import Lambdabot.Main
 import Modules (modulesInfo)
-import Prelude hiding (dropWhile)
+import Prelude hiding (dropWhile, filter)
 import System.Environment (lookupEnv)
 import System.IO.Silently (capture)
--- import Text.ParserCombinators.ReadP
-import Web.Slack
-import Web.Slack.Message
+import Web.Slack (Event(Message), Slack, SlackBot, SlackConfig(..), getId,
+  runBot, selfUserId, session, slackSelf)
+import Web.Slack.Message (sendMessage)
 
 -------------------------------------------------------------------------------
 -- Lambdabot
@@ -53,8 +51,8 @@ mkSlackConfig :: String -> SlackConfig
 mkSlackConfig apiToken = SlackConfig { _slackApiToken = apiToken }
 
 -- | Get a message if it is for \"me\".
-messageForMe :: Text -> Slack a (Maybe Text)
-messageForMe message = do
+getMessageForMe :: Text -> Slack a (Maybe Text)
+getMessageForMe message = do
   myId <- use $ session . slackSelf . selfUserId . getId
   let atMyId = "<@" <> myId <> ">"
   return $ dropWhile (== ':') <$> stripPrefix atMyId message
@@ -62,17 +60,19 @@ messageForMe message = do
 -- | Construct a @SlackBot@ from a name. This bot will pass messages addressed
 -- to it to 'lambdabot' and relay 'lambdabot''s response.
 slackBot :: SlackBot a
-slackBot (Message cid _ message _ _ _) = do
-  requestForMe <- messageForMe message
-  case requestForMe of
+slackBot (Message cid _ someMessage _ _ _) = do
+  messageForMe <- getMessageForMe someMessage
+  case messageForMe of
     Nothing -> return ()
-    Just request -> do
-      response <- liftIO $ lambdabot [unpack $ decodeHtml request]
-      sendMessage cid ("```\n" <> (pack $ decodeString response) <> "```")
+    Just message -> do
+      let request = filter (/= '`') $ decodeHtml message
+      rawResponse <- liftIO (pack . decodeString <$> lambdabot [unpack request])
+      let response = "```\n" <> rawResponse <> "```"
+      sendMessage cid response
 slackBot _ = return ()
 
-encodeHtml :: Text -> Text
-encodeHtml = toStrict . toLazyText . HTMLEncoded.text
+-- encodeHtml :: Text -> Text
+-- encodeHtml = toStrict . toLazyText . HTMLEncoded.text
 
 decodeHtml :: Text -> Text
 decodeHtml = toStrict . toLazyText . htmlEncodedText
